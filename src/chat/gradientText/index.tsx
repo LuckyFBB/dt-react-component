2a43252fca871a3709c2e09eb8638a73183800be
xinju <xinju@dtstack.com>
2026-05-28 20:10:56 +0800

    fix: remove deprecated code

--- src/chat/gradientText/index.tsx
+++ src/chat/gradientText/index.tsx
@@ -7,9 +7,7 @@ interface IGradientTextProps {
     className?: string;
     gradient?: boolean;
 }
-/**
- * @deprecated 从 rc 中同步过来，切勿直接修改该组件
- */
+
 export default function GradientText({
     className,
     gradient,
